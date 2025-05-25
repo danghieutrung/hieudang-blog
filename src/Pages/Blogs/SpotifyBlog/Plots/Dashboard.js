29c1c060920bbf7710d1d2edea49706b8caf9594
Dang Trung Hieu <hieudangtrung@protonmail.com>
2025-05-25 17:48:52 +0700

    add pbi dashboard

--- src/Pages/Blogs/SpotifyBlog/Plots/Dashboard.js
+++ src/Pages/Blogs/SpotifyBlog/Plots/Dashboard.js
@@ -4,7 +4,7 @@ import React from "react";
 const Dashboard = () => {
   return(
     <div className="plot">
-      <img src="/blog/vizzes/10_dashboard_background.jpg" alt="Dashboard" className="plot" />
+      <img src="/blog/vizzes/10_dashboard.png" alt="Dashboard" className="plot" />
     </div>
   )
 }
